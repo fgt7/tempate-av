ac3e4ac64847e0af2e82ac8452f818ee9dc3cee0
ruangimajinasi <ms.ruang.imajinasi@gmail.com>
2022-06-23 21:23:40 +0700

    Update Asupan

---
+++ data/cersex.js
@@ -0,0 +1,358 @@
+var post = [
+    {
+        "title": "Cerita Dewasa Pelajar Wanita Perawan Diperkosa Di Gubuk Sepi",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex10.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/ce65f7fe-0f27-4c91-88fe-bc012296dc0c-1582-000001f5720b1454-1-194x300.jpg",
+        "id": "10"
+    },
+    {
+        "title": "Cerita Sex Sekretaris Perawan Diperkosa Atasan",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex11.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/img_0603-1-300x221.jpg",
+        "id": "11"
+    },
+    {
+        "title": "KUPERKOSA DAN KUAMBIL KEPERAWANAN ANAK TETANGGAKU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex12.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/img_0652-223x300.jpg",
+        "id": "12"
+    },
+    {
+        "title": "CERITA MEMPERKOSA WANITA YANG DULU PERNAH MENOLAKKU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex13.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/10c7c230-d5c7-41e3-aeca-ea3d856f403b-1402-000001fe34ce871a-1-240x300.jpg",
+        "id": "13"
+    },
+    {
+        "title": "CERITA SELINGKUH DENGAN ISTRI TEMAN YG MENGAJAK BERSENANG-SENANG",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex14.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/eaa1635f-cb77-4bc5-9419-2ff95765bbf9-1402-000001fe01b31e0a-1-300x300.jpg",
+        "id": "14"
+    },
+    {
+        "title": "CERITA SEX KUPERKOSA ISTRI TETANGGAKU SETELAH NGINTIP MEREKA BERCUMBU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex15.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/59bf4963-b052-423a-a30f-a0fbe61e7eae-1369-00000283af872b0f-1-251x300.jpg",
+        "id": "15"
+    },
+    {
+        "title": "BERBEKAL OBAT BIUS, KUPERKOSA WANITA HAMIL",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex16.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/bee3f8da-f99b-4bb6-a7f6-bc929dd71a1a-1977-0000035b134dd951-1-1-300x182.jpg",
+        "id": "16"
+    },
+    {
+        "title": "KUSETUBUHI PASIENKU YANG SEDANG TAK SADAR DIRI",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex17.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/e4e345bd-a2c3-400a-ab35-ba9e02948863-3816-000005fd87f4517e-1-300x224.jpg",
+        "id": "17"
+    },
+    {
+        "title": "CERITA DEWASA KUPERKOSA DAN KUAMBIL KEPERAWANAN ANAK TETANGGAKU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex18.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/img_0609-242x300.jpg",
+        "id": "18"
+    },
+    {
+        "title": "KISAH BOKEP PERKOSAAN BERGILIR",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex19.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/09/518fbd85-1e52-49cc-aca8-6dd8248a4c5c-8243-00000da1ab4ea811-1.jpg",
+        "id": "19"
+    },
+    {
+        "title": "Lumatan Gadis SMU Bikin Sange",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex2.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/f388df3497b7c39999d28.jpg",
+        "id": "2"
+    },
+    {
+        "title": "CERITA SEKS ,MEMPERKOSA ISTRI BOSKU SEBAGAI PELAMPIASAN KARENA LAMA TAK DIGAJI",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex20.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/115ca705-79d0-4811-8923-0003f7506a44-257-0000000fd7f3f98f-1-226x300.jpg",
+        "id": "20"
+    },
+    {
+        "title": "MEMBIUS DAN MEMPERKOSA GADIS YANG PERNAH MENOLAK CINTAKU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex21.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/d33b863b-8396-4c4b-be62-421fc0cbab63-6796-0000044ad633a079-1-244x300.jpg",
+        "id": "21"
+    },
+    {
+        "title": "MEMPERKOSA SEPUPUKU SAMPAI HAMIL",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex22.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/img_1586-1-211x300.jpg",
+        "id": "22"
+    },
+    {
+        "title": "AKU DIPERKOSA ANAKKU",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex23.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/fde67e49-84ce-48ce-a11e-a9aa38f69e76-1873-000001eabae32a9e-1-204x300.jpg",
+        "id": "23"
+    },
+    {
+        "title": "PEMERKOSAAN SADIS GADIS BELIA",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex24.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/img_2621-1-212x300.jpg",
+        "id": "24"
+    },
+    {
+        "title": "GURU PERKOSA SISWI DIPERPUSTAKAAN SEKOLAH",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex25.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/10/bb416359-31fd-46d4-ad97-1a6733a5aa65-6014-00000539c4c3d7e1-1-205x300.jpg",
+        "id": "25"
+    },
+    {
+        "title": "KEPERGOK ML,CEWEK SMA DIPERKOSA 2 COWOK DIHUTAN",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex26.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/11/img_2790-1-225x300.jpg",
+        "id": "26"
+    },
+    {
+        "title": "CERITA SEX DIPERKOSA MERTUA SAAT MANDI",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex27.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/11/789254b3-a69a-4508-92d3-07f2d86b4ae8-1300-00000119644bc62c-1-200x300.jpg",
+        "id": "27"
+    },
+    {
+        "title": "CERITA SEX CEWEK CANTIK & SEMOK DI PERKOSA 3 PRIA DALAM LIFT KAMPUS",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex28.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/11/159fb02c-e425-4d14-8fa9-28d62c46e7f9-1593-000001821cfc60c3-1-300x280.jpg",
+        "id": "28"
+    },
+    {
+        "title": "CERITA SEX KUPERKOSA ADIK KANDUNGKU KARNA SUDAH DIKUASAI NAFSU SEX",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex29.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/11/fc14ca1f-9a2a-41e0-b446-83158d6b3f7d-1593-000001827168c5f7-1-300x300.jpg",
+        "id": "29"
+    },
+    {
+        "title": "Cerita Sex Perawan Hilang Hutang Ibu Lunas",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex3.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/57bbea969e5f5e044408e.jpg",
+        "id": "3"
+    },
+    {
+        "title": "CERITA ABG DIPERKOSA MALAH KETAGIHAN",
+        "kategori": "Pemerkosaan",
+        "konten": "<iframe src=\"cersex/cersex30.txt\" id=\"frame\">",
+        "poster": "http://54.36.246.81/wp-content/uploads/2017/11/img_3093-1-236x300.jpg",
+        "id": "30"
+    },
+    {
+        "title": "Cerita Dewasa Gadis Polos Menjadi Maniak Sex",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex4.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/db4bbaac83539329f6a8f.jpg",
+        "id": "4"
+    },
+    {
+        "title": "Cewek SMA yang Penuh Nafsu",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex5.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/a6dac1520e1d14d8caf5c.jpg",
+        "id": "5"
+    },
+    {
+        "title": "Cerita Dewasa Dengan Anak Sekolah Primadona SMU",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex6.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/8edbfc2f08c1b9cac6349.jpg",
+        "id": "6"
+    },
+    {
+        "title": "Belajar Sex Dengan Pacar Masih SMP",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex7.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/c305547396b27370d540d.jpg",
+        "id": "7"
+    },
+    {
+        "title": "Cerita Pesta Sex Kelulusan SMA",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex8.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/ec44e0e92284b8540beae.jpg",
+        "id": "8"
+    },
+    {
+        "title": "Ditantang Gadis ABG Ngewe",
+        "kategori": "Cerita Sex ABG",
+        "konten": "<iframe src=\"cersex/cersex9.txt\" id=\"frame\">",
+        "poster": "https://telegra.ph/file/5b04d6986e91f8f0a328a.jpg",
+        "id": "9"
+    }
+];
+	
+var postLength = post.length;
+//Empty container:
+$("#list-cerita").empty();
+//Loop:
+for (i=0; i<postLength; i++) {
+  //Create thumb structure:
+  var listItem =
+    '<div id="cersex">'+
+      '<a class="button" href="'+post[i].id+'" data-obj="'+i+'">'+
+      '<div class="card" style="width:100%">'+
+      '<img src="'+post[i].poster+'" class="card-img-top" width="100%" height="130px" alt="">'+
+      '<div class="card-body">'+
+      '<div class="judul-post"><h7><b>'+ post[i].title+'</b></h7>'+
+      '</div>'+
+      '</a>' +
+    '</div>';
+  $("#list-cerita").append(listItem);
+};
+
+
+var thisElement = 0;
+
+function innerContent(content){
+  $(".inner-title").html(post[content].title);
+  $(".inner-poster").attr("src",post[content].poster);
+  $(".inner-konten").html(post[content].konten);
+  $(".inner-tanggal").html(post[content].tanggal);
+};
+
+//Open post:
+$(".button").click(function(e){
+  e.preventDefault();
+  thisElement = $(this).attr("data-obj");
+  innerContent(thisElement);
+  $("#search-bar").css({"display":"none"});
+  $("#list-cerita").css({"display":"none"});
+  $(".slider").css({"display":"none"});
+  $("#cersex-lainnya").css({"display":"none"});
+  $("#post-blog").css({"display":"block"});
+  $("#judul-awal").css({"display":"none"});
+  $(window).scrollTop(0);
+  dissBtn();
+});
+
+$(".prev-post").on("click", function() {
+    $(window).scrollTop(0);
+});
+
+$(".next-post").on("click", function() {
+    $(window).scrollTop(0);
+});
+
+$(".close-post").on("click", function() {
+    $(window).scrollTop(0);
+});
+
+//Close post:
+$(".close-post, .modal-sandbox").click(function(){
+  $("#search-bar").css({"display":"block"});
+  $("#list-cerita").css({"display":"block"});
+  $("#cersex-lainnya").css({"display":"block"});
+  $("#post-blog").css({"display":"none"});
+  $("#judul-awal").css({"display":"block"});
+  $("#list-cerita").css({"width":"100%"});
+  $("#list-cerita").css({"display":"grid"});
+  $("#list-cerita").css({"grid-template-rows":"185px auto"});
+  $("#list-cerita").css({"grid-template-columns":"50% auto"});
+  $("#list-cerita").css({"grid-row-gap":"10px"});
+  $("#list-cerita").css({"grid-column-gap":"10px"});
+});
+
+//Next post:
+$(".next-post").click(function(e){
+  e.preventDefault();
+  if (thisElement<postLength-1) {
+    thisElement = parseInt(thisElement) + 1;
+    innerContent(thisElement);
+    dissBtn();
+  };
+});
+
+//Prev post:
+$(".prev-post").click(function(e){
+  e.preventDefault();
+  if (thisElement>0) {
+    thisElement = parseInt(thisElement) - 1;
+    innerContent(thisElement);
+    dissBtn();
+  };
+});
+
+//Button disable:
+function dissBtn(){
+  $(".prev-post, .next-post").removeClass("disabled");
+  if (thisElement<=0){
+    $(".prev-post").addClass("disabled");
+  }
+  else if (thisElement>=postLength-1){
+    $(".next-post").addClass("disabled");
+  };
+};
+
+$(document).ready(function(){
+
+      var list = $("#list-cerita #cersex");
+      var numToShow = 6;
+      var button = $("#cersex-lainnya");
+      var numInList = list.length;
+      list.hide();
+      if (numInList > numToShow) {
+        button.show();
+      }
+      list.slice(0, numToShow).show();
+
+      button.click(function(){
+          var showing = list.filter(':visible').length;
+          list.slice(showing - 1, showing + numToShow).fadeIn();
+          var nowShowing = list.filter(':visible').length;
+          if (nowShowing >= numInList) {
+            button.hide();
+          }
+      });
+
+});
+
+
+$(function() {
+  $("#cariCersex").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    if (value.length) {
+      $("#list-cerita #cersex").filter(function() {
+        $("#list-cerita").show();
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    } else {
+      $("#list-cerita").hide();
+    }
+  });
+});
+
+$("#cariCersex").on("click", function() {
+    $("#list-cerita").css({"grid-template-rows":"185px auto"});
+});
+
+$("#cersex-lainnya").on("click", function() {
+  $("#list-cerita").css({"width":"100%"});
+  $("#list-cerita").css({"display":"grid"});
+  $("#list-cerita").css({"grid-template-rows":"185px auto"});
+  $("#list-cerita").css({"grid-template-columns":"50% auto"});
+  $("#list-cerita").css({"grid-row-gap":"10px"});
+  $("#list-cerita").css({"grid-column-gap":"10px"});
+});
