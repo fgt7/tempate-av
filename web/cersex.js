9834a2a2c2e5c4df3b7982eebab996d01838b4dc
ruangimajinasi <ms.ruang.imajinasi@gmail.com>
2022-07-21 12:14:41 +0700

    Update Asupan

--- web/cersex.js
+++ web/cersex.js
@@ -1,360 +1,180 @@
-var post = [
+$(function () {
+  let container = $('#page-cersex');
+  container.pagination({
+  pageSize: 30,
+  showPageNumbers: false,
+  showNavigator: true,
+  formatNavigator: 'Hal. ke-<span style="color: #f00"><b><%= currentPage %></span></b>, <b><%= totalPage %></b> Halaman, <b><%= totalNumber %></b> Cersex',
+  showGoInput: true,
+  showGoButton: true,
+  formatGoInput: 'Menuju halaman ke- <%= input %>',
+  position: 'top',
+  className: 'paginationjs-theme-blue',
+  dataSource: [
     {
-        "title": "Cerita Dewasa Pelajar Wanita Perawan Diperkosa Di Gubuk Sepi",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/029.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/029.jpg",
-        "id": "29"
+        "title": "Gadis SMA Binal",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Gadis-SMA-Binal-06-27"
     },
     {
-        "title": "Cerita Sex Sekretaris Perawan Diperkosa Atasan",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/028.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/028.jpg",
-        "id": "28"
+        "title": "Aku Tidak Munafik",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Aku-Tidak-Munafik-01-02"
     },
     {
-        "title": "KUPERKOSA DAN KUAMBIL KEPERAWANAN ANAK TETANGGAKU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/027.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/027.jpg",
-        "id": "27"
+        "title": "Merasakan Memek Indah Dokter Muda Yang Masi Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Merasakan-Memek-Indah-Dokter-Muda-Yang-Masi-Perawan-01-22"
     },
     {
-        "title": "CERITA MEMPERKOSA WANITA YANG DULU PERNAH MENOLAKKU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/026.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/026.jpg",
-        "id": "26"
+        "title": "Kentu Perawan di Tonton Oleh Istri",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Kentu-Perawan-di-Tonton-Oleh-Istri-01-23"
     },
     {
-        "title": "CERITA SELINGKUH DENGAN ISTRI TEMAN YG MENGAJAK BERSENANG-SENANG",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/025.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/025.jpg",
-        "id": "25"
+        "title": "Cerita Mesum Ngentot Mahasiswi Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Cerita-Mesum-Ngentot-Mahasiswi-Perawan-01-23"
     },
     {
-        "title": "CERITA SEX KUPERKOSA ISTRI TETANGGAKU SETELAH NGINTIP MEREKA BERCUMBU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/024.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/024.jpg",
-        "id": "24"
+        "title": "Membeli Gadis Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Membeli-Gadis-Perawan-01-24"
     },
     {
-        "title": "BERBEKAL OBAT BIUS, KUPERKOSA WANITA HAMIL",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/023.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/023.jpg",
-        "id": "23"
+        "title": "Percintaan Perawan Nakal",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Percintaan-Perawan-Nakal-01-24"
     },
     {
-        "title": "KUSETUBUHI PASIENKU YANG SEDANG TAK SADAR DIRI",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/022.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/022.jpg",
-        "id": "22"
+        "title": "Berpura Pura Menolak di Perkosa",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Berpura-Pura-Menolak-di-Perkosa-01-25"
     },
     {
-        "title": "CERITA DEWASA KUPERKOSA DAN KUAMBIL KEPERAWANAN ANAK TETANGGAKU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/021.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/021.jpg",
-        "id": "21"
+        "title": "Dapat Prawan ABG Tentangga Gara Gara BF",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Dapat-Prawan-ABG-Tentangga-Gara-Gara-BF-01-25"
     },
     {
-        "title": "KISAH BOKEP PERKOSAAN BERGILIR",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/020.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/020.jpg",
-        "id": "20"
+        "title": "Merelakan Perawannya",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Merelakan-Perawannya-01-25"
     },
     {
-        "title": "Lumatan Gadis SMU Bikin Sange",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/019.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/019.jpg",
-        "id": "19"
+        "title": "Perawanku Telah Hilang Dengan Om Om",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Perawanku-Telah-Hilang-Dengan-Om-Om-01-27"
     },
     {
-        "title": "CERITA SEKS ,MEMPERKOSA ISTRI BOSKU SEBAGAI PELAMPIASAN KARENA LAMA TAK DIGAJI",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/018.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/018.jpg",
-        "id": "18"
+        "title": "Pesta Sex Perawan Bispak",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Pesta-Sex-Perawan-Bispak-01-27"
     },
     {
-        "title": "MEMBIUS DAN MEMPERKOSA GADIS YANG PERNAH MENOLAK CINTAKU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/017.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/017.jpg",
-        "id": "17"
+        "title": "Perawan Buat Adikku",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Perawan-Buat-Adikku-01-28"
     },
     {
-        "title": "MEMPERKOSA SEPUPUKU SAMPAI HAMIL",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/016.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/016.jpg",
-        "id": "16"
+        "title": "Kehilangan Perawan Saat Umur 14 Tahun",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Kehilangan-Perawan-Saat-Umur-14-Tahun-01-28"
     },
     {
-        "title": "AKU DIPERKOSA ANAKKU",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/015.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/015.jpg",
-        "id": "15"
+        "title": "Terdahsyat Ngentotin Perawan Putri Sulung",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Terdahsyat-Ngentotin-Perawan-Putri-Sulung-01-29"
     },
     {
-        "title": "PEMERKOSAAN SADIS GADIS BELIA",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/014.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/014.jpg",
-        "id": "14"
+        "title": "Calon Istri Pamanku yang Masih Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Calon-Istri-Pamanku-yang-Masih-Perawan-01-29"
     },
     {
-        "title": "GURU PERKOSA SISWI DIPERPUSTAKAAN SEKOLAH",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/013.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/013.jpg",
-        "id": "13"
+        "title": "Membeli Keperawanan Gadis SMP",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Membeli-Keperawanan-Gadis-SMP-01-29"
     },
     {
-        "title": "KEPERGOK ML,CEWEK SMA DIPERKOSA 2 COWOK DIHUTAN",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/012.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/012.jpg",
-        "id": "12"
+        "title": "Atasanku yang Berhasil Merenggut Keperawananku",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Atasanku-yang-Berhasil-Merenggut-Keperawananku-01-30"
     },
     {
-        "title": "CERITA SEX DIPERKOSA MERTUA SAAT MANDI",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/011.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/011.jpg",
-        "id": "11"
+        "title": "Ngentot Dengan Cewek Imut Yang Masih Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Ngentot-Dengan-Cewek-Imut-Yang-Masih-Perawan-01-30"
     },
     {
-        "title": "CERITA SEX CEWEK CANTIK & SEMOK DI PERKOSA 3 PRIA DALAM LIFT KAMPUS",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/010.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/010.jpg",
-        "id": "10"
+        "title": "Perawanku Di Ambil Suami Baru Ibuku",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Perawanku-Di-Ambil-Suami-Baru-Ibuku-01-30"
     },
     {
-        "title": "CERITA SEX KUPERKOSA ADIK KANDUNGKU KARNA SUDAH DIKUASAI NAFSU SEX",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/009.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/009.jpg",
-        "id": "9"
+        "title": "Merawanin Anak Pembantu Di Rumahku",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Merawanin-Anak-Pembantu-Di-Rumahku-01-30"
     },
     {
-        "title": "Cerita Sex Perawan Hilang Hutang Ibu Lunas",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/008.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/008.jpg",
-        "id": "8"
+        "title": "Penggila Seks Hampir 20 Wanita Ku Tiduri",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Penggila-Seks-Hampir-20-Wanita-Ku-Tiduri-01-30"
     },
     {
-        "title": "CERITA ABG DIPERKOSA MALAH KETAGIHAN",
-        "kategori": "Pemerkosaan",
-        "konten": "<iframe src=\"web/cersex/007.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/007.jpg",
-        "id": "7"
+        "title": "Bro Tolong Hamili Istri Gua",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Bro-Tolong-Hamili-Istri-Gua-01-31"
     },
     {
-        "title": "Cerita Dewasa Gadis Polos Menjadi Maniak Sex",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/006.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/006.jpg",
-        "id": "6"
+        "title": "Pembantu Montok Yang Suka ML Denganku",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Pembantu-Montok-Yang-Suka-ML-Denganku-01-31"
     },
     {
-        "title": "Cewek SMA yang Penuh Nafsu",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/005.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/005.jpg",
-        "id": "5"
+        "title": "Cerita Ngentot 2 Cewek Liar Minta Di Rangsang",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Cerita-Ngentot-2-Cewek-Liar-Minta-Di-Rangsang-01-31"
     },
     {
-        "title": "Cerita Dewasa Dengan Anak Sekolah Primadona SMU",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/004.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/004.jpg",
-        "id": "4"
+        "title": "Pembantu Perawan Gatel Minta Dientot",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Pembantu-Perawan-Gatel-Minta-Dientot-02-01"
     },
     {
-        "title": "Belajar Sex Dengan Pacar Masih SMP",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/003.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/003.jpg",
-        "id": "3"
+        "title": "Malam PertamaKu Pecah Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Malam-PertamaKu-Pecah-Perawan-02-01"
     },
     {
-        "title": "Cerita Pesta Sex Kelulusan SMA",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/002.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/002.jpg",
-        "id": "2"
+        "title": "Jual Perawan Pada Om Hidung Belang Yang Banyak Uang",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Jual-Perawan-Pada-Om-Hidung-Belang-Yang-Banyak-Uang-02-01"
     },
     {
-        "title": "Ditantang Gadis ABG Ngewe",
-        "kategori": "Cerita Sex ABG",
-        "konten": "<iframe src=\"web/cersex/001.txt\" id=\"frame\">",
-        "poster": "web/cersex/thumb/001.jpg",
-        "id": "1"
+        "title": "Kesempatan Dalam Kesempitan Setubuhi Gadis Pingsan Masih Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Kesempatan-Dalam-Kesempitan-Setubuhi-Gadis-Pingsan-Masih-Perawan-02-01"
+    },
+    {
+        "title": "Ternyata Cewekku Masih Perawan",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Ternyata-Cewekku-Masih-Perawan-02-02"
+    },
+    {
+        "title": "Keperawanku Direnggut Suami Orang",
+        "kategori": "Cersex Perawan",
+        "link": "https://telegra.ph/Keperawanku-Direnggut-Suami-Orang-02-02"
     }
-];
-	
-var postLength = post.length;
-//Empty container:
-$("#list-cerita").empty();
-//Loop:
-for (i=0; i<postLength; i++) {
-  //Create thumb structure:
-  var listItem =
-    '<div id="cersex">'+
-      '<a class="button" href="'+post[i].id+'" data-obj="'+i+'">'+
-      '<div style="width:100%">'+
-      '<img src="'+post[i].poster+'" alt="' + post[i].title + '" class="card-img-top" width="100%" height="130px" alt="">'+
-      '<div class="card-body">'+
-      '<div class="judul-cersex">'+ post[i].title+'</div>'+
-      '</a>' +
-    '</div>';
-  $("#list-cerita").append(listItem);
-};
+  ],
+  
+  callback: function (data, pagination) {
+      var dataHtml = '';
+      $.each(data, function (index, item) {
+          dataHtml += '<div class="row sort-item"><a href="http://adf.ly/19888345/' + item.link + '" target="_blank" style="color:black"><p id="judul-cersex" align="left">' + item.title + '</p><p style="margin-left:10px">'+ item.kategori +'</p></div>';
+          });
 
-
-var thisElement = 0;
-
-function innerContent(content){
-  $(".inner-title").html(post[content].title);
-  $(".inner-poster").attr("src",post[content].poster);
-  $(".inner-konten").html(post[content].konten);
-  $(".inner-tanggal").html(post[content].tanggal);
-};
-
-//Open post:
-$(".button").click(function(e){
-  e.preventDefault();
-  thisElement = $(this).attr("data-obj");
-  innerContent(thisElement);
-  $("#search-cersex").css({"display":"none"});
-  $("#list-cerita").css({"display":"none"});
-  $("#cersex-lainnya").css({"display":"none"});
-  $("#post-blog").css({"display":"block"});
-  $("#goTop").css({"display":"none"});
-  $(".float").css({"display":"none"});
-  $(".donasi").css({"display":"none"});
-  $(window).scrollTop(0);
-  dissBtn();
-});
-
-$(".prev-post").on("click", function() {
-    $(window).scrollTop(0);
-});
-
-$(".next-post").on("click", function() {
-    $(window).scrollTop(0);
-});
-
-$(".close-post").on("click", function() {
-    $(window).scrollTop(0);
-});
-
-//Close post:
-$(".close-post, .modal-sandbox").click(function(){
-  $("#search-cersex").css({"display":"block"});
-  $("#list-cerita").css({"display":"block"});
-  $("#cersex-lainnya").css({"display":"block"});
-  $("#post-blog").css({"display":"none"});
-  $("#list-cerita").css({"width":"100%"});
-  $("#list-cerita").css({"display":"grid"});
-  $("#list-cerita").css({"grid-template-rows":"175px auto"});
-  $("#list-cerita").css({"grid-template-columns":"50% auto"});
-  $("#list-cerita").css({"grid-row-gap":"10px"});
-  $("#list-cerita").css({"grid-column-gap":"10px"});
-  $("#goTop").css({"display":"block"});
-  $(".float").css({"display":"block"});
-  $(".donasi").css({"display":"block"});
-});
-
-//Next post:
-$(".next-post").click(function(e){
-  e.preventDefault();
-  if (thisElement<postLength-1) {
-    thisElement = parseInt(thisElement) + 1;
-    innerContent(thisElement);
-    dissBtn();
-  };
-});
-
-//Prev post:
-$(".prev-post").click(function(e){
-  e.preventDefault();
-  if (thisElement>0) {
-    thisElement = parseInt(thisElement) - 1;
-    innerContent(thisElement);
-    dissBtn();
-  };
-});
-
-//Button disable:
-function dissBtn(){
-  $(".prev-post, .next-post").removeClass("disabled");
-  if (thisElement<=0){
-    $(".prev-post").addClass("disabled");
-  }
-  else if (thisElement>=postLength-1){
-    $(".next-post").addClass("disabled");
-  };
-};
-
-$(document).ready(function(){
-
-      var list = $("#list-cerita #cersex");
-      var numToShow = 10;
-      var button = $("#cersex-lainnya");
-      var numInList = list.length;
-      list.hide();
-      if (numInList > numToShow) {
-        button.show();
-      }
-      list.slice(0, numToShow).show();
-
-      button.click(function(){
-          var showing = list.filter(':visible').length;
-          list.slice(showing - 1, showing + numToShow).fadeIn();
-          var nowShowing = list.filter(':visible').length;
-          if (nowShowing >= numInList) {
-            button.hide();
-          }
-      });
-
-});
-
-
-$(function() {
-  $("#cariCersex").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    if (value.length) {
-      $("#list-cerita #cersex").filter(function() {
-        $("#list-cerita").show();
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-    } else {
-      $("#list-cerita").hide();
-    }
-  });
-});
-
-$("#cariCersex").on("click", function() {
-    $("#list-cerita").css({"grid-template-rows":"175px auto"});
-});
-
-$("#cersex-lainnya").on("click", function() {
-  $("#list-cerita").css({"width":"100%"});
-  $("#list-cerita").css({"display":"grid"});
-  $("#list-cerita").css({"grid-template-rows":"175px auto"});
-  $("#list-cerita").css({"grid-template-columns":"50% auto"});
-  $("#list-cerita").css({"grid-row-gap":"20px"});
-  $("#list-cerita").css({"grid-column-gap":"10px"});
-});
+          $("#data-cersex").html(dataHtml);
+        }
+    })
+  })
