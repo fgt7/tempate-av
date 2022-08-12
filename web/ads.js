7adafc63eda2334d125937c2d940cd8e78eaefce
ruangimajinasi <ms.ruang.imajinasi@gmail.com>
2022-08-12 17:58:17 +0700

    Update Asupan

--- web/ads.js
+++ web/ads.js
@@ -2303,7 +2303,7 @@ const ads = [
   
 function randomads() {
   let random = ads[Math.floor(Math.random() * ads.length)];
-  iklan.innerHTML = `<a href="${random.link}"><img src="${random.photo}" class="rounded" width="100%"><div class="container-fluid full-width-image" style="margin-top:-50px"><h1>${random.nama}</h1></div></div><div class="modal-footer" style="border-style:none;margin-top:-5px"><img class="position-absolute bottom-0 start-50 translate-middle-x" src="assets/img/buy.png" height="60px"></a>`;
+  iklan.innerHTML = `<a href="${random.link}"><img src="${random.photo}" class="rounded" width="100%"><div class="container-fluid full-width-image"><h1>${random.nama}</h1></div></div><div class="modal-footer" style="border-style:none;margin-top:-6px"><img class="position-absolute bottom-0 start-50 translate-middle-x" src="assets/img/buy.png" height="60px"></a>`;
   ads.innerHTML = random.ads;
 }
 randomads();
